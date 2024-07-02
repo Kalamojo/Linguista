80513209b148eb39e8b73749892026fa60ba3753
Kalamojo <kojo22444488888888@gmail.com>
2024-07-02 12:07:09 -0500

    Passing numpy array

--- integrate-chat-ui.py
+++ integrate-chat-ui.py
@@ -69,41 +69,41 @@ def on_input_change():
     # st.session_state.generated.append(str(grammar_response)) # right now just adding string version of grammar response
 
 
-# def load_audio(file: tuple[str, bytes], sr: int = 16000):
-#     """
-#     Open an audio file and read as mono waveform, resampling as necessary
+def load_audio(file: tuple[str, bytes], sr: int = 16000):
+    """
+    Open an audio file and read as mono waveform, resampling as necessary
 
-#     Parameters
-#     ----------
-#     file: (str, bytes)
-#         The audio file to open or bytes of audio file
+    Parameters
+    ----------
+    file: (str, bytes)
+        The audio file to open or bytes of audio file
 
-#     sr: int
-#         The sample rate to resample the audio if necessary
+    sr: int
+        The sample rate to resample the audio if necessary
 
-#     Returns
-#     -------
-#     A NumPy array containing the audio waveform, in float32 dtype.
-#     """
+    Returns
+    -------
+    A NumPy array containing the audio waveform, in float32 dtype.
+    """
     
-#     if isinstance(file, bytes):
-#         inp = file
-#         file = 'pipe:'
-#     else:
-#         inp = None
+    if isinstance(file, bytes):
+        inp = file
+        file = 'pipe:'
+    else:
+        inp = None
     
-#     try:
-#         # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
-#         # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
-#         out, _ = (
-#             ffmpeg.input(file, threads=0)
-#             .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
-#             .run(cmd="ffmpeg", capture_stdout=True, capture_stderr=True, input=inp)
-#         )
-#     except ffmpeg.Error as e:
-#         raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+    try:
+        # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
+        # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
+        out, _ = (
+            ffmpeg.input(file, threads=0)
+            .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
+            .run(cmd="ffmpeg", capture_stdout=True, capture_stderr=True, input=inp)
+        )
+    except ffmpeg.Error as e:
+        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
 
-#     return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
+    return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
 
 # Function to convert audio to text w/ Whisper
 def convert_audio_to_text(audio_data, language_code):
@@ -111,14 +111,14 @@ def convert_audio_to_text(audio_data, language_code):
     #     tmp_file.write(audio_data)
     #     tmp_file_path = tmp_file.name
 
-    # audio = whisper.load_audio(tmp_file_path)
-    # audio = whisper.pad_or_trim(audio)
+    audio = whisper.load_audio(tmp_file_path)
+    audio = whisper.pad_or_trim(audio)
     # mel = whisper.log_mel_spectrogram(audio).to(model.device)
     #mel = whisper.log_mel_spectrogram(load_audio(audio_data)).to(model.device)
     #options = whisper.DecodingOptions(language=language_code)
     #result = whisper.decode(model, mel, options)
-    audio_file = io.BytesIO(audio_data)
-    result = model.transcribe(audio_file, language=language_code)
+    #audio_file = io.BytesIO(audio_data)
+    result = model.transcribe(audio, language=language_code)
     return result.text
 
 def recording_callback():
