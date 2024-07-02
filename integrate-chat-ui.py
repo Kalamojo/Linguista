ddd69515b1d819a83ea5be86d1a01e91b04e7430
Kalamojo <kojo22444488888888@gmail.com>
2024-07-01 22:19:10 -0500

    Added language selection

--- integrate-chat-ui.py
+++ integrate-chat-ui.py
@@ -3,7 +3,7 @@ from streamlit_chat import message
 from streamlit_mic_recorder import mic_recorder
 from openai import OpenAI
 import whisper
-import language_tool_python
+# import language_tool_python
 import tempfile
 import json
 import os
@@ -14,23 +14,29 @@ openai_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_key)
 
 
-language_code = 'es' # Spanish
-language = "Spanish"
-
-
 model = whisper.load_model("base")
-tool = language_tool_python.LanguageToolPublicAPI(language_code)
+# tool = language_tool_python.LanguageToolPublicAPI(language_code)
 
 def on_btn_click():
     del st.session_state.past[:]
     del st.session_state.generated[:]
 
 # Function to get model response
-def get_model_response(user_input):
+def get_model_response(user_input, language):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-                {"role": "system", "content": f"Respond to the user's dialogue in {language}. Ensure your response encourages the user to continue the conversation by asking simple and relevant follow-up questions, allowing them to lead the discussion. Next, provide a response in English that summarizes LanguageTool's findings about the user's input. Use simple terms to explain any grammatical errors the learner made, why these errors are incorrect in the context of {language} grammar, and how to correct them. Include detailed examples to illustrate the correct usage in the context of the conversation. Focus on natural speech patterns in {language}, unless the learner explicitly requests a different focus. Keep feedback concise yet helpful and split into manageable parts if there are multiple errors."},
+                {"role": "system", "content": f"""
+                    Respond to the user's dialogue in {language}. Ensure your response encourages the 
+                    user to continue the conversation by asking simple and relevant follow-up questions, 
+                    allowing them to lead the discussion. Next, provide a response in English that summarizes 
+                    LanguageTool's findings about the user's input. Use simple terms to explain any grammatical 
+                    errors the learner made, why these errors are incorrect in the context of {language} grammar, 
+                    and how to correct them. Include detailed examples to illustrate the correct usage in the 
+                    context of the conversation. Focus on natural speech patterns in {language}, unless the learner 
+                    explicitly requests a different focus. Keep feedback concise yet helpful and split into manageable 
+                    parts if there are multiple errors.
+                    """},
                 {"role": "user", "content": user_input}
             ],
         
@@ -38,9 +44,9 @@ def get_model_response(user_input):
     print(response)
     return response.choices[0].message.content
 
-def get_languagetool_response(user_input):
-    matches = tool.check(user_input)
-    return [json.dumps(match.__dict__) for match in matches]
+# def get_languagetool_response(user_input):
+#     matches = tool.check(user_input)
+#     return [json.dumps(match.__dict__) for match in matches]
 
 st.session_state.setdefault(
     'past', 
@@ -53,17 +59,18 @@ st.session_state.setdefault(
 
 def on_input_change():
     user_input = st.session_state.user_input
+    language = st.session_state.lang_name
     print("bruh something happened", user_input, "fr")
-    model_response = get_model_response(user_input)
-    grammar_response = get_languagetool_response(user_input)
-    print(grammar_response)
+    model_response = get_model_response(user_input, language)
+    # grammar_response = get_languagetool_response(user_input)
+    # print(grammar_response)
     st.session_state.past.append(user_input)
     st.session_state.generated.append(model_response)
-    st.session_state.generated.append(str(grammar_response)) # right now just adding string version of grammar response
+    # st.session_state.generated.append(str(grammar_response)) # right now just adding string version of grammar response
 
 
 # Function to convert audio to text w/ Whisper
-def convert_audio_to_text(audio_data):
+def convert_audio_to_text(audio_data, language_code):
     with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmp_file:
         tmp_file.write(audio_data)
         tmp_file_path = tmp_file.name
@@ -77,8 +84,9 @@ def convert_audio_to_text(audio_data):
 
 def recording_callback():
     if st.session_state.my_recorder_output:
+        language_code = language = st.session_state.lang_code
         audio_bytes = st.session_state.my_recorder_output['bytes']
-        audio_text = convert_audio_to_text(audio_bytes)
+        audio_text = convert_audio_to_text(audio_bytes, language_code)
         st.session_state.user_input = audio_text
         on_input_change()
         st.rerun()
@@ -86,12 +94,9 @@ def recording_callback():
 chat_placeholder = st.empty()
 
 with chat_placeholder.container():
-    ind = 0    
     for i in range(len(st.session_state['past'])):                
         message(st.session_state['past'][i], is_user=True, key=f"{i}_user")
-        message(st.session_state['generated'][ind], is_user=False, key=f"{ind}")
-        message(st.session_state['generated'][ind+1], is_user=False, key=f"{ind+1}")
-        ind += 2
+        message(st.session_state['generated'][i], is_user=False, key=f"{i}")
     
     st.button("Clear message", on_click=on_btn_click)
 
@@ -100,3 +105,8 @@ with st.container():
     st.title("Language Chat")
     mic_recorder(key='my_recorder', callback=recording_callback)
     st.text_input("User Input:", on_change=on_input_change, key="user_input")
+    c1, c2 = st.columns(2)
+    with c1:
+        st.text_input("What is the language code?", value="es", key="lang_code")
+    with c2:
+        st.text_input("What is the language name?", value="Spanish", key="lang_name")
