76e25aa9e33964e415ea7f9dc98d549c39305b2d
Kalamojo <kojo22444488888888@gmail.com>
2024-07-02 11:37:48 -0500

    Using audio bytes directly instead of saving

--- integrate-chat-ui.py
+++ integrate-chat-ui.py
@@ -66,15 +66,52 @@ def on_input_change():
     # st.session_state.generated.append(str(grammar_response)) # right now just adding string version of grammar response
 
 
+def load_audio(file: tuple[str, bytes], sr: int = 16000):
+    """
+    Open an audio file and read as mono waveform, resampling as necessary
+
+    Parameters
+    ----------
+    file: (str, bytes)
+        The audio file to open or bytes of audio file
+
+    sr: int
+        The sample rate to resample the audio if necessary
+
+    Returns
+    -------
+    A NumPy array containing the audio waveform, in float32 dtype.
+    """
+    
+    if isinstance(file, bytes):
+        inp = file
+        file = 'pipe:'
+    else:
+        inp = None
+    
+    try:
+        # This launches a subprocess to decode audio while down-mixing and resampling as necessary.
+        # Requires the ffmpeg CLI and `ffmpeg-python` package to be installed.
+        out, _ = (
+            ffmpeg.input(file, threads=0)
+            .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
+            .run(cmd="ffmpeg", capture_stdout=True, capture_stderr=True, input=inp)
+        )
+    except ffmpeg.Error as e:
+        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
+
+    return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
+
 # Function to convert audio to text w/ Whisper
 def convert_audio_to_text(audio_data, language_code):
-    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmp_file:
-        tmp_file.write(audio_data)
-        tmp_file_path = tmp_file.name
+    # with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmp_file:
+    #     tmp_file.write(audio_data)
+    #     tmp_file_path = tmp_file.name
 
-    audio = whisper.load_audio(tmp_file_path)
-    audio = whisper.pad_or_trim(audio)
-    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+    # audio = whisper.load_audio(tmp_file_path)
+    # audio = whisper.pad_or_trim(audio)
+    # mel = whisper.log_mel_spectrogram(audio).to(model.device)
+    mel = whisper.log_mel_spectrogram(load_audio(audio_data)).to(model.device)
     options = whisper.DecodingOptions(language=language_code)
     result = whisper.decode(model, mel, options)
     return result.text
